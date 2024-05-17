22bc2ec0b5e390fbdbfae491f7c894a61540ce6f
CLDOA <145026689+GATAIML@users.noreply.github.com>
2024-05-18 01:10:11 +0530

    18-05-2024

---
+++ js/avinya.js
@@ -0,0 +1,45 @@
+function animateElements() {
+  const elements = document.querySelectorAll('.animate-fade-in');
+  let delay = 0;
+
+  elements.forEach((element, index) => {
+      setTimeout(() => {
+          element.classList.add('active');
+      }, delay);
+
+      delay += 300; 
+  });
+}
+
+window.addEventListener('load', animateElements);
+
+
+ScrollReveal().reveal('.project-container', {
+  duration: 1000,
+  delay: 200,
+  easing: 'ease-in-out',
+  distance: '100px',
+  origin: 'bottom'
+});
+ScrollReveal().reveal('.roles-container', {
+  duration: 1000,
+  delay: 200,
+  easing: 'ease-in-out',
+  distance: '100px',
+  origin: 'bottom'
+});
+ScrollReveal().reveal('.contact-container', {
+  duration: 1000,
+  delay: 200,
+  easing: 'ease-in-out',
+  distance: '100px',
+  origin: 'bottom'
+});
+ScrollReveal().reveal('.footer-container', {
+  duration: 1000,
+  delay: 200,
+  easing: 'ease-in-out',
+  distance: '100px',
+  origin: 'bottom'
+});
+
